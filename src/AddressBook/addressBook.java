6cd251fb46f624875e167a3319e5daabf5168efa
PhilippProzorov <uberphil2000@gmail.com>
2019-05-27 20:49:55 +0300

    Implemented size() and search() methods, also added tests for them. Additional cosmetic changes. There may be a need for final cosmetic revision since everything else is functioning just fine.

--- src/AddressBook/addressBook.java
+++ src/AddressBook/addressBook.java
@@ -2,36 +2,36 @@ package AddressBook;
 import java.util.*;
 
 class AddressBook {
-    private Map<String, Address> habitantInfo =
+    private Map<String, Address> inhabitantInfo =
             new HashMap<>();
     Map<String, Address> get() {
-        return habitantInfo;
+        return inhabitantInfo;
     }
 
     void add(String inhabitant, Address address) {
-        if ((inhabitant.isEmpty()) || (habitantInfo.containsKey(inhabitant))) {
+        if ((inhabitant.isEmpty()) || (inhabitantInfo.containsKey(inhabitant))) {
             throw new IllegalArgumentException("FAILED TO ADD. INCORRECT INPUT");
         }
-        else habitantInfo.put(inhabitant, address);
+        inhabitantInfo.put(inhabitant, address);
     }
 
-    public void search(String inhabitant) {
-        if (habitantInfo.containsKey(inhabitant)) {
-            habitantInfo.get(inhabitant);
+    public String search(String inhabitant) {
+        if (inhabitantInfo.containsKey(inhabitant)) {
+            return inhabitantInfo.get(inhabitant).toString();
         }
-        else throw new IllegalArgumentException("NO RESULT. EITHER INCORRECT INPUT OR THIS PERSON DOESN'T EXIST");
+        throw new IllegalArgumentException("NO RESULT. EITHER INCORRECT INPUT OR THIS PERSON DOESN'T EXIST");
     }
 
     void change(String inhabitant, Address address) {
-        if (habitantInfo.containsKey(inhabitant)) {
-            habitantInfo.replace(inhabitant, address);
+        if (inhabitantInfo.containsKey(inhabitant)) {
+            inhabitantInfo.replace(inhabitant, address);
         }
         else throw new IllegalArgumentException("FAILED TO CHANGE. NON-EXISTENT INHABITANT ");
     }
 
-    void remove(String habitant) {
-        if (habitantInfo.containsKey(habitant)) {
-            habitantInfo.remove(habitant);
+    void remove(String inhabitant) {
+        if (inhabitantInfo.containsKey(inhabitant)) {
+            inhabitantInfo.remove(inhabitant);
         }
         else throw new IllegalArgumentException("FAILED TO REMOVE. THIS PERSON DOESN'T EXIST");
     }
@@ -39,7 +39,7 @@ class AddressBook {
     List<String> sameStreet(String street) {
         List<String> streetInhabitants =
                 new ArrayList<>();
-        for (Map.Entry<String, Address> addressBook : habitantInfo.entrySet()) {
+        for (Map.Entry<String, Address> addressBook : inhabitantInfo.entrySet()) {
             if (Objects.equals(addressBook.getValue().getStreet(), street)) {
                 streetInhabitants.add(addressBook.getKey());
             }
@@ -50,12 +50,16 @@ class AddressBook {
     List<String> sameHouse(String street, int house) {
         List<String> houseInhabitants =
                 new ArrayList<>();
-        for (Map.Entry<String, Address> addressBook : habitantInfo.entrySet()) {
+        for (Map.Entry<String, Address> addressBook : inhabitantInfo.entrySet()) {
             if (Objects.equals(addressBook.getValue().getStreet(), street) &&
                     (Objects.equals(addressBook.getValue().getHouse(), house))) {
                 houseInhabitants.add(addressBook.getKey());
             }
         }
         return houseInhabitants;
+    }
+
+    public int size() {
+        return inhabitantInfo.size();
     }
 }
